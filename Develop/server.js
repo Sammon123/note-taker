f50f0dfd7fa2325d5d530b10596a7501c106fead
Peter Sammon <petersammon2@gmail.com>
2020-06-14 15:59:01 -0400

    pseudocode server.js

---
+++ Develop/server.js
@@ -0,0 +1,8 @@
+// wire up express app
+// require dependencies
+// pull the contents from the db.json file into this file
+// set up express app
+// handling the data parsing
+// ROUTES
+// create route so that when a person makes a get request to notes, they receive the notes page
+// start server to begin listening
